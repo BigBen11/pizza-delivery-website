c49d3d8dd27b15b0c707a935c6aeaf30f89ede3b
Bogdan Polskiy <bogdan.polskiy@stud.h-da.de>
2024-06-06 15:41:11 +0200

    fix: Pizzen verschwinden (erst) beim Bäcker, wenn der Fahrer die entsprechende Bestellung auf *unterwegs setzt

--- src/Praktikum/Prak2/baecker.php
+++ src/Praktikum/Prak2/baecker.php
@@ -16,7 +16,7 @@ class Baecker extends Page
     protected function processReceivedData():void
     {
         parent::processReceivedData();
-        
+
             if(isset($_POST['status'])){
                 foreach ($_POST['status'] as $orderedArticleId => $status) {
                     $query = "UPDATE `ordered_article` SET `status` = ? WHERE `ordered_article_id` = ?";
@@ -35,7 +35,7 @@ class Baecker extends Page
     $query = "SELECT `ordered_article`.`ordered_article_id`, `article`.`name`, `ordered_article`.`status` 
               FROM `ordered_article` 
               JOIN `article` ON `ordered_article`.`article_id` = `article`.`article_id`
-              WHERE `ordered_article`.`status` = 1 OR `ordered_article`.`status` = 2 "; // Nur Pizzen abrufen, die nicht "Fertig" sind
+              WHERE `ordered_article`.`status` = 1 OR `ordered_article`.`status` = 2  OR `ordered_article`.`status` = 3"; // Nur Pizzen abrufen, die nicht "Fertig" sind
 
     $result = $this->db->query($query);
     $pizzas = [];
